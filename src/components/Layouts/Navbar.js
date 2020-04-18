2bba5061b7820cb02ad583d13084e9e7e1be43c3
Melanie Rogoff <mgrogoff@gmail.com>
2020-04-17 23:36:26 -0700

    Starting basic layout

---
+++ src/components/Layouts/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+function NavBar() {
+
+    const location = useLocation();//code from react-router to check routes anytime the user uses a link to navigate
+
+    return (
+      <div className="navbar">
+            <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>Home</Link>
+            <Link to="/About" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>About</Link>
+            <Link to="/Contact" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>Contact</Link>
+            <Link to="/Signup" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>Sign Up</Link>
+            <Link to="/Login" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>Log In</Link>
+      </div>
+
+    )
+}
+
+export default NavBar;
