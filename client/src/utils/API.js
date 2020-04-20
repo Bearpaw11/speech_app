919faac0684e8d91b18426894686ca1d2b1960f6
David Coons <dlc9005@gmail.com>
2020-04-19 21:35:54 -0700

    added event listeners for form, still need to post to db

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,22 +1,31 @@
-const db = require("../models");
-const passport = require("../config/passport");
+// const db = require("../../../models");
+// const passport = require("./config/passport");
+import axios from 'axios'
+// import express from 'express'
+// const app = express()
 
-module.exports = function (app) {
+
+export default {
 
 
-    app.post("/api/signup", function (req, res) {
-        db.Users.create({
-            email: req.body.email,
-            password: req.body.password,
-            username: req.body.username
-        })
-            .then(function () {
-                res.redirect(307, "/");
-            })
-            .catch(function (err) {
-                res.status(401).json(err);
-            });
-    });
+
+    signUp: function (userData) {
+        return axios.post("api/signup", userData)
+    }
+
+//    signup: app.post("/api/signup", function (req, res) {
+//         db.Users.create({
+//             email: req.body.email,
+//             password: req.body.password,
+//             username: req.body.username
+//         })
+//             .then(function () {
+//                 res.redirect(307, "/");
+//             })
+//             .catch(function (err) {
+//                 res.status(401).json(err);
+//             });
+//     })
 
 
 
