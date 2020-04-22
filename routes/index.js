1bdf3702ee12e01c4bcaac6271381bd764b7cc49
David Coons <dlc9005@gmail.com>
2020-04-21 23:13:56 -0700

    got server and db working again

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,6 @@
 const path = require("path");
 const db = require("../models");
-<<<<<<< HEAD
-const passport = ("../config/passport")
-
-=======
-const passport = require("../config/passport");
->>>>>>> 686c11125f1cb01483aad501cbbe85f75053baf3
+// const passport = require("../config/passport");
 // const apiRoute = require("./api-routes")
 
 
@@ -32,50 +27,35 @@ module.exports = function (app) {
                 res.status(401).json(err);
             });
     });
-    app.post("/api/login", passport.authenticate("local"), function(req, res) {
-        res.json(req.user);
-    });
+    // app.post("/api/login/", passport.authenticate("local"), function(req, res) {
+    //     res.json(req.user);
+    // });
 
-    app.post("/api/login", passport.authenticate("local"), function (req, res) {
-        res.json(req.user);
-    });
+    
 
-    app.get("/logout", function (req, res) {
-        req.logout();
-        res.redirect("/");
-    });
+    // app.get("/logout/", function (req, res) {
+    //     req.logout();
+    //     res.redirect("/");
+    // });
 
-    app.get("/api/user_data", function (req, res) {
-        if (!req.user) {
-            // The user is not logged in, send back an empty object
-            res.json({});
-        } else {
-            // Otherwise send back the user's email and id
-            // Sending back a password, even a hashed password, isn't a good idea
-            res.json({
-                username: req.user.username,
-                email: req.user.email,
-                id: req.user.id
-            });
-        }
-    });
+    
 
 
     
 
-    app.get("/api/user_data", function(req, res) {
-        if (!req.user) {
-            // The user is not logged in, send back an empty object
-            res.json({});
-        } else {
-            // Otherwise send back the user's email and id
-            // Sending back a password, even a hashed password, isn't a good idea
-            res.json({
-                username: req.user.username,
-                email: req.user.email,
-                id: req.user.id
-            });
-        }
-    });
+    // app.get("/api/user_data", function(req, res) {
+    //     if (!req.user) {
+    //         // The user is not logged in, send back an empty object
+    //         res.json({});
+    //     } else {
+    //         // Otherwise send back the user's email and id
+    //         // Sending back a password, even a hashed password, isn't a good idea
+    //         res.json({
+    //             username: req.user.username,
+    //             email: req.user.email,
+    //             id: req.user.id
+    //         });
+    //     }
+    // });
 };
 
