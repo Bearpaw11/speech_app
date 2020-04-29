b7c6bdbda69b56a25499ed54d983e7839f69c98d
Melanie Rogoff <mgrogoff@gmail.com>
2020-04-29 16:54:44 -0700

    Added delete button

--- client/src/components/Pages/Members.js
+++ client/src/components/Pages/Members.js
@@ -57,9 +57,11 @@ if(this.state.loggedIn){
 
                     <ul className="recordingList">
                         <li className="recordingListItem">
+                            <button type="button" id="speechDeleter">Delete</button>
                             {/* MAP THROUGH ARRAY OF RECORDINGS*/}
                             {/* HAVE AUTOMATIC GENERATION OF LISTS & ANALYTICS HERE */}
                             {/* WHEN TITLE IS CLICKED, HAVE ACCORDION THAT DISPLAYS ANALYTICS  */}
+                            {/* Have delete button generated next to each recording */}
                         </li>
                     </ul>
                 
