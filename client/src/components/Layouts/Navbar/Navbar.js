815ea14b01e2a7598bfd30282508d0d165e53a7d
Cake <mcgrail.shauna@gmail.com>
2020-04-30 21:15:45 -0700

    I updated the css for the navbar and updated navbar.js class names

--- client/src/components/Layouts/Navbar/Navbar.js
+++ client/src/components/Layouts/Navbar/Navbar.js
@@ -2,10 +2,9 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import API from '../../../utils/API'
 import './navbar.css';
-
+import logo from './colorwave.png';
 function NavBar(props) {
   const location = useLocation();
-
     function logOut() {
         API.logOut().then((logout) => {
             console.log(logout)
@@ -13,50 +12,37 @@ function NavBar(props) {
             alert("You've been logged out!")
         })
     }
-    
-
   return (
-    <div>
+    <div className="navi">
         <nav className="navbar navbar-expand-md">
-        
-            <a className="navbar-brand" href="#">Navigation</a>
-
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
                 <span className="fas fa-bars fa-1x"></span>
             </button>
-
-            {/* Navbar links */}
             <div className="collapse navbar-collapse" id="collapsibleNavbar">
+                <div className="navPic"></div>
+            <img className="navPic" src={logo} />
                 <ul className="navbar-nav">
-
                     <li className="nav-item">
                         <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>Home</Link>
                     </li>
-
                     <li className="nav-item">
                         <Link to="/About" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>About</Link>
                     </li>
-
                     <li className="nav-item">
                         <Link to="/Contact" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>Contact</Link>
                     </li>
-
                     <li className="nav-item">
                         <Link to="/SignUp" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>Sign Up</Link>
                     </li>
-
                     {/* <li className="nav-item">
                         <Link to="/Recordings" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>Recordings</Link>
                     </li>
-
                     <li className="nav-item">
                         <Link to="/Members" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>Members</Link>
                     </li> */}
-                    
                     <li className="nav-item">
                         <a id="navlink" onClick={props.showModal}>Log In</a>
                     </li>
-
                     <li className="nav-item">
                         <a id="logOut" onClick={logOut} >Log Out</a>
                     </li>
@@ -64,8 +50,6 @@ function NavBar(props) {
             </div>
         </nav>
     </div>
-    
   )
 }
-
 export default NavBar;
