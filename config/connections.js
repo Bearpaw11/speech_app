423c8fd9632eb4529ccbc0b5fbf431554de0df61
Melanie Rogoff <mgrogoff@gmail.com>
2020-05-05 21:20:02 -0700

    Carousel fix

--- config/connections.js
+++ config/connections.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
         host: "localhost",
         port: 3306,
         user: "root",
-        password: "",
+        password: "ilovemusic3",
         database: "speechapp"
     });
 }
